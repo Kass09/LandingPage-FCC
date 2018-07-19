ab8fcbddb68b676c57133101b7e4a9445445d6b1
Kass_ <kevinsanjuan@hotmail.com>
2018-07-19 18:36:50 -0500

    Changes-update-logoMenu

--- assets/js/test.js
+++ assets/js/test.js
@@ -4,8 +4,10 @@ $(document).ready(function(){
         var scrollDistance = $(window).scrollTop();
         if (scrollDistance > navOffset) {
             $('#nav-bar').addClass("nav-sticky navbar-dark");
+            $('#header-img').addClass("sticky-img");
         } else{
             $('#nav-bar').removeClass("nav-sticky navbar-dark");
+            $('#header-img').removeClass("sticky-img");
         }
     })
 });
